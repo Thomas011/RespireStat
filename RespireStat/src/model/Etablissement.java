9955f1d5ffd312bd12b919814fb5381e82369ac7
Thomas011 <45967420+Thomas011@users.noreply.github.com>
2019-04-11 16:46:18 +0200

    constructeur etbalissement et lieu

--- RespireStat/src/model/Etablissement.java
+++ RespireStat/src/model/Etablissement.java
@@ -3,4 +3,9 @@ package model;
 public class Etablissement {
 	private String identifiant;
 	private String nom_etablissement;
+	
+	public Etablissement (String i, String n) {
+		this.identifiant = i;
+		this.nom_etablissement = n;
+	}
 }
--- RespireStat/src/model/Lieu.java
+++ RespireStat/src/model/Lieu.java
@@ -5,4 +5,10 @@ public class Lieu {
 	private String code_postal;
 	private String departement;
 	private String ville;
+	
+	public Lieu (String c, String d, String v) {
+		this.code_postal = c;
+		this.departement = d;
+		this.ville = v;
+	}
 }
